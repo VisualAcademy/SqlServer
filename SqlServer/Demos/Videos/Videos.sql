8628c5157a53149ab2d5aae6d5dff9ba6599d4d8
WebCampKR <webcamp@outlook.kr>
2019-07-05 03:28:40 +0900

    Videos 테이블에 Url 열 추가

--- SqlServer/Demos/Videos/Videos.sql
+++ SqlServer/Demos/Videos/Videos.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[Videos]
 (
 	Id INT NOT NULL PRIMARY KEY Identity(1, 1),
 	Title NVarChar(Max) Not Null,
-	Created DateTimeOffset(7) Default(SysDateTimeOffset() AT TIME ZONE 'Korea Standard Time')
+	Created DateTimeOffset(7) Default(SysDateTimeOffset() AT TIME ZONE 'Korea Standard Time'),
+	Url NVarChar(Max) Null
 )
 Go

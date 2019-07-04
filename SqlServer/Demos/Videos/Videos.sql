09ccd599bea60fda3b938fbd65cb9f7f6c322290
RedPlus <redplus@redplus.net>
2019-07-05 03:15:09 +0900

    Add project files.

---
+++ SqlServer/Demos/Videos/Videos.sql
@@ -0,0 +1,8 @@
+﻿-- 비디오 테이블
+CREATE TABLE [dbo].[Videos]
+(
+	Id INT NOT NULL PRIMARY KEY Identity(1, 1),
+	Title NVarChar(Max) Not Null,
+	Created DateTimeOffset(7) Default(SysDateTimeOffset() AT TIME ZONE 'Korea Standard Time')
+)
+Go

d720a6545c7c30b2a252ac5251951b71d8619bad
RedPlus <redplus@redplus.net>
2019-07-18 21:34:07 +0900

    20190718_PostgreSQL_02_Tables_Crud_테이블 만들고 CRUD 연습하기

---
+++ SqlServer/PostgreSQL/02_Tables_Crud.sql
@@ -0,0 +1,23 @@
+﻿-- 데이터베이스 생성
+Create Database DatabaseApp;
+
+--[0] 테이블(모델, 엔터티) 생성
+Create Table Tables (
+    Id      INT Not Null Generated ALWAYS As IDENTITY Primary Key, 
+    Note    Text Null
+);
+
+--[1] 입력(Create)
+INSERT INTO Tables (Note) Values('안녕하세요.');
+--[2] 출력(Read, 조회)
+Select * From Tables;
+--[3] 수정(Update, 갱신)
+Update Tables Set Note = '반갑습니다.' Where Id = 1;
+--[4] 삭제(Delete)
+DELETE FROM Tables Where Id = 1;
+
+--[5] 테이블 삭제
+Drop Table Tables;
+
+--[6] 데이터베이스 삭제
+Drop Database databaseapp;

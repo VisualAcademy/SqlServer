aca57dd7ed6c39776d8407cfb8135039043d428e
Park YongJune <redplus@redplus.net>
2022-02-06 04:32:51 +0900

    Broths, Noodles, Garnishes 테이블 추가

---
+++ SqlServer/BuffetApp/Buffets.sql
@@ -0,0 +1,27 @@
+﻿Use BuffetApp
+Go
+
+-- 국물
+Create Table Broths (
+	[Id] Int Not Null Identity, 
+	[Name] NVarChar(100) Not Null,
+	[IsVegan] Bit Not Null,
+	Constraint [PK_Broths] Primary Key ([Id])
+);
+-- 국수
+Create Table Noodles (
+	[Id] Int Not Null Identity, 
+	[Name] NVarChar(100) Not Null,
+	[BrothId] Int Null,
+	Constraint [PK_Noodles] Primary Key ([Id]),
+	Constraint [FK_Noodles_Broths_BrothId] Foreign Key ([BrothId]) References [Broths] ([Id])
+);
+-- 고명
+Create Table Garnishes (
+	[Id] Int Not Null Identity, 
+	[Name] NVarChar(100) Not Null,
+	--[Calories] Decimal(18,2) Not Null,
+	[NoodleId] Int Null,
+	Constraint [PK_Garnishes] Primary Key ([Id]),
+	Constraint [FK_Garnishes_Noodles_NoodleId] Foreign Key ([NoodleId]) References [Noodles] ([Id])
+);

d18139ac5b4d6823e0628590852f40bea49d03a2
Park YongJune <redplus@redplus.net>
2024-05-06 22:38:52 +0900

    Create Person table

---
+++ src/SqlServer/dbo/Tables/Person.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Person
+(
+    PersonId INT IDENTITY(1, 1) PRIMARY KEY,
+    FirstName NVARCHAR(128) NOT NULL,
+    MiddleInitial NVARCHAR(10) NULL,
+    LastName NVARCHAR(128) NOT NULL,
+    DateOfBirth DATE NOT NULL DEFAULT(GetDate())
+)
+Go

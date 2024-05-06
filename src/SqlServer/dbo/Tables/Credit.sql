08520dc22801f53fa3e89615b468f94ed90223da
Park YongJune <redplus@redplus.net>
2024-05-06 22:40:47 +0900

    Create Credit table

---
+++ src/SqlServer/dbo/Tables/Credit.sql
@@ -0,0 +1,13 @@
+﻿-- Create Credit table
+CREATE TABLE Credit
+(
+    StudentId INT REFERENCES Student (StudentId),
+    CourseId INT REFERENCES Course (CourseId),
+    Grade DECIMAL(5, 2) CHECK (Grade <= 100.00),
+    Attempt TINYINT,
+    CONSTRAINT [UQ_studentgrades] UNIQUE CLUSTERED
+    (
+        StudentId, CourseId, Grade, Attempt
+    )
+)
+Go

557e45a50939b7884d5a5b9979f3923ef7558b28
Park YongJune <redplus@redplus.net>
2024-11-23 21:08:54 +0900

    Posts sample table add

---
+++ src/SqlServer/dbo/Tables/Posts.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [Posts] (
+     [Id] int NOT NULL IDENTITY,
+     [Title] nvarchar(max) NOT NULL,
+     [Contents] nvarchar(max) NOT NULL,
+     [Tags] nvarchar(max) NOT NULL,
+     [Visits] nvarchar(max) NOT NULL,
+     CONSTRAINT [PK_Posts] PRIMARY KEY ([Id])
+);
